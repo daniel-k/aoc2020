ad97fd7652682f79c317b65a9a3a1f9cd109c694
Daniel Krebs <github@daniel-krebs.net>
2020-12-14 20:02:49 +0100

    day 13: add Python solution

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 from itertools import tee
+from typing import Iterable, List
 
+def flatten(iterable: Iterable) -> List:
+    return [e for sub_iterable in iterable for e in sub_iterable]
 
 # from more-itertools:
 # https://more-itertools.readthedocs.io/en/stable/_modules/more_itertools/recipes.html#pairwise
+
+
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b = tee(iterable)
